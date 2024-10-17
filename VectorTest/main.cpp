9e499fb9804f83ed625f41ba84b791331fd195ae
DK-So <deniz.k.sogukpinar@protonmail.com>
2024-10-17 20:16:01 +0200

    Initial Commit

---
+++ VectorTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VectorTest
+//
+//  Created by Deniz Kemal Soğukpınar on 17.10.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
